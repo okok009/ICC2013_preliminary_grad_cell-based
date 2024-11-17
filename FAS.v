1dab046d5310fd16dfa3fcaa4c02397b6956e46b
okok009 <https://github.com/okok009>
2024-11-17 17:02:20 +0800

    FFT update
    Find w0, w4 can be more easier to calculate. But still have error.

--- FAS.v
+++ FAS.v
@@ -96,14 +96,14 @@ assign fft_valid = fft_valid_reg;
 assign fft_valid_valid = fft_valid_valid_reg;
 assign fft_d0 = fft_d0_16<<16;
 
-FFT_4point_16 #(.w1_real(w0_real) , .w1_imag (w0_imag) , .w2_real(w4_real) , .w2_imag (w4_imag) , .w3_real(w0_real) , .w3_imag (w0_imag)) FFT_L_1(fft_X0_reg , fft_X4_reg , fft_X8_reg , fft_X12_reg , mid_1 , mid_5 , mid_9 , mid_13);
-FFT_4point_16 #(.w1_real(w1_real) , .w1_imag (w1_imag) , .w2_real(w5_real) , .w2_imag (w5_imag) , .w3_real(w2_real) , .w3_imag (w2_imag)) FFT_L_2(fft_X1_reg , fft_X5_reg , fft_X9_reg , fft_X13_reg , mid_2 , mid_6 , mid_10 , mid_14);
-FFT_4point_16 #(.w1_real(w2_real) , .w1_imag (w2_imag) , .w2_real(w6_real) , .w2_imag (w6_imag) , .w3_real(w4_real) , .w3_imag (w4_imag)) FFT_L_3(fft_X2_reg , fft_X6_reg , fft_X10_reg , fft_X14_reg , mid_3 , mid_7 , mid_11 , mid_15);
-FFT_4point_16 #(.w1_real(w3_real) , .w1_imag (w3_imag) , .w2_real(w7_real) , .w2_imag (w7_imag) , .w3_real(w6_real) , .w3_imag (w6_imag)) FFT_L_4(fft_X3_reg , fft_X7_reg , fft_X11_reg , fft_X15_reg , mid_4 , mid_8 , mid_12 , mid_16);
-FFT_4point_16 #(.w1_real(w0_real) , .w1_imag (w0_imag) , .w2_real(w4_real) , .w2_imag (w4_imag) , .w3_real(w0_real) , .w3_imag (w0_imag)) FFT_R_1(mid_1 , mid_2 ,mid_3 , mid_4 , fft_d0_16 , fft_d8 , fft_d4 , fft_d12);
-FFT_4point_32 #(.w1_real(w0_real) , .w1_imag (w0_imag) , .w2_real(w4_real) , .w2_imag (w4_imag) , .w3_real(w0_real) , .w3_imag (w0_imag)) FFT_R_2(mid_5 , mid_6 ,mid_7 , mid_8 , fft_d2 , fft_d10 , fft_d6 , fft_d14);
-FFT_4point_32 #(.w1_real(w0_real) , .w1_imag (w0_imag) , .w2_real(w4_real) , .w2_imag (w4_imag) , .w3_real(w0_real) , .w3_imag (w0_imag)) FFT_R_3(mid_9 , mid_10 ,mid_11 , mid_12 , fft_d1 , fft_d9 , fft_d5 , fft_d13);
-FFT_4point_32 #(.w1_real(w0_real) , .w1_imag (w0_imag) , .w2_real(w4_real) , .w2_imag (w4_imag) , .w3_real(w0_real) , .w3_imag (w0_imag)) FFT_R_4(mid_13 , mid_14 ,mid_15 , mid_16 , fft_d3 , fft_d11 , fft_d7 , fft_d15);
+FFT_4point_16_w0_w4 FFT_L_1(fft_X0_reg, fft_X4_reg, fft_X8_reg, fft_X12_reg, mid_1, mid_5, mid_9, mid_13);
+FFT_4point_16 #(.w1_real(w1_real), .w1_imag (w1_imag), .w2_real(w5_real), .w2_imag (w5_imag), .w3_real(w2_real), .w3_imag (w2_imag)) FFT_L_2(fft_X1_reg, fft_X5_reg, fft_X9_reg, fft_X13_reg, mid_2, mid_6, mid_10, mid_14);
+FFT_4point_16_w4 #(.w1_real(w2_real), .w1_imag (w2_imag), .w2_real(w6_real), .w2_imag (w6_imag)) FFT_L_3(fft_X2_reg, fft_X6_reg, fft_X10_reg, fft_X14_reg, mid_3, mid_7, mid_11, mid_15);
+FFT_4point_16 #(.w1_real(w3_real), .w1_imag (w3_imag), .w2_real(w7_real), .w2_imag (w7_imag), .w3_real(w6_real), .w3_imag (w6_imag)) FFT_L_4(fft_X3_reg, fft_X7_reg, fft_X11_reg, fft_X15_reg, mid_4, mid_8, mid_12, mid_16);
+FFT_4point_16_w0_w4 FFT_R_1(mid_1, mid_2, mid_3, mid_4, fft_d0_16, fft_d8, fft_d4, fft_d12);
+FFT_4point_32 FFT_R_2(mid_5, mid_6, mid_7, mid_8, fft_d2, fft_d10, fft_d6, fft_d14);
+FFT_4point_32 FFT_R_3(mid_9, mid_10, mid_11, mid_12, fft_d1, fft_d9, fft_d5, fft_d13);
+FFT_4point_32 FFT_R_4(mid_13, mid_14, mid_15, mid_16, fft_d3, fft_d11, fft_d7, fft_d15);
 
 // FFT_seq
 always @(posedge clk) begin
@@ -156,6 +156,56 @@ end
 
 endmodule
 
+module FFT_2point_32_w0(X, Y, fft_a, fft_b);
+input [31:0] X;
+input [31:0] Y;
+output [31:0] fft_a;
+output [31:0] fft_b;
+
+wire signed [15:0] a, b, c, d;
+wire signed [15:0] fft_a_real_cal, fft_a_imag_cal;
+wire signed [15:0] fft_b_real_cal, fft_b_imag_cal;
+
+assign a = X[31-:16];
+assign b = X[15-:16];
+assign c = Y[31-:16];
+assign d = Y[15-:16];
+
+assign fft_a_real_cal = a + c;
+assign fft_a_imag_cal = b + d;
+
+assign fft_b_real_cal = (a - c);
+assign fft_b_imag_cal = (b - d);
+
+assign fft_a = {fft_a_real_cal, fft_a_imag_cal};
+assign fft_b = {fft_b_real_cal, fft_b_imag_cal};
+endmodule
+
+module FFT_2point_32_w4(X, Y, fft_a, fft_b);
+input [31:0] X;
+input [31:0] Y;
+output [31:0] fft_a;
+output [31:0] fft_b;
+
+wire signed [15:0] a, b, c, d;
+wire signed [15:0] fft_a_real_cal, fft_a_imag_cal;
+wire signed [15:0] fft_b_real_cal, fft_b_imag_cal;
+
+assign a = X[31-:16];
+assign b = X[15-:16];
+assign c = Y[31-:16];
+assign d = Y[15-:16];
+
+assign fft_a_real_cal = a + c;
+assign fft_a_imag_cal = b + d;
+
+assign fft_b_real_cal = (b - d);
+assign fft_b_imag_cal = (c - a);
+
+assign fft_a = {fft_a_real_cal, fft_a_imag_cal};
+assign fft_b = {fft_b_real_cal, fft_b_imag_cal};
+endmodule
+
 module FFT_2point_32(X, Y, fft_a, fft_b);
 parameter signed [31:0] w_real = 0;
 parameter signed [31:0] w_imag = 0;
@@ -181,6 +231,50 @@ assign fft_b_imag_cal = (a - c) * w_imag + (b - d) * w_real;
 
 assign fft_a = {fft_a_real_cal, fft_a_imag_cal};
 assign fft_b = {fft_b_real_cal[47], fft_b_real_cal[30:16], fft_b_imag_cal[47], fft_b_imag_cal[30:16]};
+endmodule
+
+module FFT_2point_16_w0(X, Y, fft_a, fft_b);
+input [15:0] X;
+input [15:0] Y;
+output [15:0] fft_a;
+output [31:0] fft_b;
+
+wire signed [15:0] a, c;
+wire signed [15:0] fft_a_real_cal;
+wire signed [15:0] fft_b_real_cal;
+
+assign a = X;
+assign c = Y;
+
+assign fft_a_real_cal = a + c;
+
+assign fft_b_real_cal = a - c;
+
+assign fft_a = {fft_a_real_cal};
+assign fft_b = {fft_b_real_cal, 16'b0};
+endmodule
+
+module FFT_2point_16_w4(X, Y, fft_a, fft_b);
+parameter signed [31:0] w_real = 0;
+parameter signed [31:0] w_imag = 0;
+input [15:0] X;
+input [15:0] Y;
+output [15:0] fft_a;
+output [31:0] fft_b;
+
+wire signed [15:0] a, c;
+wire signed [15:0] fft_a_real_cal;
+wire signed [47:0] fft_b_real_cal, fft_b_imag_cal;
+
+assign a = X;
+assign c = Y;
+
+assign fft_a_real_cal = a + c;
+
+assign fft_b_imag_cal = c-a;
+
+assign fft_a = {fft_a_real_cal};
+assign fft_b = {16'b0, fft_b_imag_cal};
 endmodule
 
 module FFT_2point_16(X, Y, fft_a, fft_b);
@@ -210,21 +304,49 @@ endmodule
 // w1 w3
 // w2 w3
 module FFT_4point_32(in_1, in_2, in_3, in_4, out_1, out_2, out_3, out_4);
-parameter signed [31:0] w1_real = 32'b0;
-parameter signed [31:0] w1_imag = 32'b0;  
-parameter signed [31:0] w2_real = 32'b0;
-parameter signed [31:0] w2_imag = 32'b0;
-parameter signed [31:0] w3_real = 32'b0;
-parameter signed [31:0] w3_imag = 32'b0;
 input signed [31:0] in_1, in_2, in_3, in_4;
 output signed [31:0] out_1, out_2, out_3, out_4;
 
 wire signed [31:0] out_1_1_1, out_1_1_2, out_1_2_1, out_1_2_2;
 
-FFT_2point_32 #(.w_real(w1_real), .w_imag(w1_imag)) FFT_1_1(.X(in_1), .Y(in_3), .fft_a(out_1_1_1), .fft_b(out_1_1_2));
-FFT_2point_32 #(.w_real(w2_real), .w_imag(w2_imag)) FFT_1_2(.X(in_2), .Y(in_4), .fft_a(out_1_2_1), .fft_b(out_1_2_2));
-FFT_2point_32 #(.w_real(w3_real), .w_imag(w3_imag)) FFT_2_1(.X(out_1_1_1), .Y(out_1_2_1), .fft_a(out_1), .fft_b(out_2));
-FFT_2point_32 #(.w_real(w3_real), .w_imag(w3_imag)) FFT_2_2(.X(out_1_1_2), .Y(out_1_2_2), .fft_a(out_3), .fft_b(out_4));
+FFT_2point_32_w0 FFT_1_1(.X(in_1), .Y(in_3), .fft_a(out_1_1_1), .fft_b(out_1_1_2));
+FFT_2point_32_w4 FFT_1_2(.X(in_2), .Y(in_4), .fft_a(out_1_2_1), .fft_b(out_1_2_2));
+FFT_2point_32_w0 FFT_2_1(.X(out_1_1_1), .Y(out_1_2_1), .fft_a(out_1), .fft_b(out_2));
+FFT_2point_32_w0 FFT_2_2(.X(out_1_1_2), .Y(out_1_2_2), .fft_a(out_3), .fft_b(out_4));
+
+endmodule
+
+module FFT_4point_16_w0_w4(in_1, in_2, in_3, in_4, out_1, out_2, out_3, out_4);
+input signed [15:0] in_1, in_2, in_3, in_4;
+output signed [15:0] out_1;
+output signed [31:0] out_2, out_3, out_4;
+
+wire signed [15:0] out_1_1_1, out_1_2_1;
+wire signed [31:0] out_1_1_2, out_1_2_2;
+
+FFT_2point_16_w0 FFT_1_1(.X(in_1), .Y(in_3), .fft_a(out_1_1_1), .fft_b(out_1_1_2));
+FFT_2point_16_w4 FFT_1_2(.X(in_2), .Y(in_4), .fft_a(out_1_2_1), .fft_b(out_1_2_2));
+FFT_2point_16_w0 FFT_2_1(.X(out_1_1_1), .Y(out_1_2_1), .fft_a(out_1), .fft_b(out_2));
+FFT_2point_32_w0 FFT_2_2(.X(out_1_1_2), .Y(out_1_2_2), .fft_a(out_3), .fft_b(out_4));
+    
+endmodule
+
+module FFT_4point_16_w4(in_1, in_2, in_3, in_4, out_1, out_2, out_3, out_4);
+parameter signed [31:0] w1_real = 32'b0;
+parameter signed [31:0] w1_imag = 32'b0;  
+parameter signed [31:0] w2_real = 32'b0;
+parameter signed [31:0] w2_imag = 32'b0;
+input signed [15:0] in_1, in_2, in_3, in_4;
+output signed [15:0] out_1;
+output signed [31:0] out_2, out_3, out_4;
+
+wire signed [15:0] out_1_1_1, out_1_2_1;
+wire signed [31:0] out_1_1_2, out_1_2_2;
+
+FFT_2point_16 #(.w_real(w1_real), .w_imag(w1_imag)) FFT_1_1(.X(in_1), .Y(in_3), .fft_a(out_1_1_1), .fft_b(out_1_1_2));
+FFT_2point_16 #(.w_real(w2_real), .w_imag(w2_imag)) FFT_1_2(.X(in_2), .Y(in_4), .fft_a(out_1_2_1), .fft_b(out_1_2_2));
+FFT_2point_16_w4 FFT_2_1(.X(out_1_1_1), .Y(out_1_2_1), .fft_a(out_1), .fft_b(out_2));
+FFT_2point_32_w4 FFT_2_2(.X(out_1_1_2), .Y(out_1_2_2), .fft_a(out_3), .fft_b(out_4));
     
 endmodule
 
